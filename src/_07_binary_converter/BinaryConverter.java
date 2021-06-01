2ab800589b9d6d4ed1f68bef667bcf6df7e56baa
suryanig <67238881+suryanig@users.noreply.github.com>
2021-05-31 17:37:55 -0700

    I finished Binary Converter which means that I finished level 1 module 1

--- src/_07_binary_converter/BinaryConverter.java
+++ src/_07_binary_converter/BinaryConverter.java
@@ -14,21 +14,26 @@ public class BinaryConverter implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JTextField answer = new JTextField(20);
-	JLabel lable = new JLabel();
-	JButton button = new JButton();
+	JLabel label = new JLabel();
+	JButton button = new JButton("convert");
 
 	void setup() {
 		frame.add(panel);
 		panel.add(answer);
-		answer.add(button);
-		button.add(lable);
+		panel.add(button);
+		panel.add(label);
 		button.addActionListener(this);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-
+String asciiChar = convert(answer.getText());
+label.setText(asciiChar);
+frame.pack();
 	}
 
 	String convert(String input) {
--- src/_07_binary_converter/BinaryConverterRunner.java
+++ src/_07_binary_converter/BinaryConverterRunner.java
@@ -2,6 +2,6 @@ package _07_binary_converter;
 
 public class BinaryConverterRunner {
 	public static void main(String[] args) {
-
+		new BinaryConverter().setup();
 	}
 }
